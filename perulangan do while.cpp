974a67af8c66560aaf7fc6a81cb304a90c0c3bda
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-03-18 14:57:17 +0700

    perulangan do while

---
+++ perulangan do while.cpp
@@ -0,0 +1,14 @@
+#include <Iostream>
+using namespace std;
+
+int main(){
+     char pilihan;
+    do(
+        cout << "Berangkat ambil takjil" << endl;
+        cout << "Antri takjil" << endl;
+        cout << "Ambil takjil" << endl;
+        cout << "Apakah mau antri kembali ?" << endl;
+        cin >> pilihan;
+
+    )while(pilihan == 'y' || pilihan == 'y')
+}
